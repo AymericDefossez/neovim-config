b12d1eb79bc368e5f8b9225f1d95b2a4e140e11d
Aymeric Defossez <aymeric.defossez@e-mothep.com>
2024-09-25 14:27:20 +0200

    reset config

--- lua/plugins/copilot.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'github/copilot.vim',
-  name = 'Copilot',
-  config = function ()
-    local map = vim.keymap.set
-    map('i', '<C-Right>', '<Plug>(copilot-accept-word)')
-  end
-}
--- lua/plugins/scope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "tiagovla/scope.nvim",
-    opts = {},
-    config = function()
-      require("telescope").load_extension("scope")
-    end
-  }
-}
