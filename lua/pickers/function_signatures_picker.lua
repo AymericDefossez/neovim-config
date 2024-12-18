8af790cb792957f8ffbae3f549af2a652007bcbb
Aymeric Defossez <aymeric.defossez@e-mothep.com>
2024-12-18 15:06:51 +0100

    add preview and test file

--- lua/pickers/function_signatures_picker.lua
+++ lua/pickers/function_signatures_picker.lua
@@ -11,8 +11,6 @@ local function build_function_signature(name_node, accessor_node, params_node, t
   local params = params_node and vim.treesitter.get_node_text(params_node, bufnr) or "()"
   local return_type = type_node and vim.treesitter.get_node_text(type_node, bufnr) or ""
 
-  print("name: " .. name .. ", accessor: " .. accessor .. ", params: " .. params .. ", type: " .. return_type)
-
   if accessor ~= "" and return_type ~= "" then
     return string.format("%s %s%s%s", accessor, name, params, return_type)
   elseif accessor ~= "" then
@@ -25,22 +23,14 @@ local function build_function_signature(name_node, accessor_node, params_node, t
 end
 
 local function get_functions()
-  local parser = vim.treesitter.get_parser(0)
+  local parser = vim.treesitter.get_parser(0, vim.bo.filetype)
   local tree = parser:parse()[1]
   local root = tree:root()
 
   local functions = {}
-  local query = vim.treesitter.query.parse(
+  local method_definition_query = vim.treesitter.query.parse(
     vim.bo.filetype,
     [[
-    ; Capturer les déclarations de fonctions
-    ;(function_declaration
-    ;  (accessibility_modifier) @accessor
-    ;  name: (identifier) @name
-    ;  parameters: (formal_parameters) @params
-    ;  type: (type_annotation)? @type)
-
-    ; Capturer les méthodes dans les classes
     (method_definition
       (accessibility_modifier)? @accessor
       name: (property_identifier) @name
@@ -49,20 +39,22 @@ local function get_functions()
     ]]
   )
 
-  for _, match in query:iter_matches(root, 0) do
+  for _, match, _ in method_definition_query:iter_matches(root, 0) do
     local accessor_node = match[1]
     local name_node = match[2]
     local params_node = match[3]
     local type_node = match[4]
+    local parent_node = name_node:parent()
     local signature = build_function_signature(name_node, accessor_node, params_node, type_node, 0)
-
     local range = { name_node:range() }
+
     print("Function: " .. signature .. " at " .. range[1] .. ":" .. range[2])
     table.insert(functions, {
       name = signature,
       line = range[1] + 1,
       col = range[2] + 1,
       bufnr = vim.api.nvim_get_current_buf(),
+      parent_node = parent_node,
     })
   end
 
@@ -70,31 +62,18 @@ local function get_functions()
 end
 
 local function preview_function(self, entry)
-  local line = entry.value.line
   local bufnr = entry.value.bufnr
+  local parent_node = entry.value.parent_node
 
-  local parser = vim.treesitter.get_parser(bufnr)
-  local tree = parser:parse()[1]
-  local root = tree:root()
-
-  local target_node = nil
-  for node in root:iter_children() do
-    print("node type :" .. node:type() .. " at " .. node:range())
-    local start_row, _, end_row, _ = node:range()
-    if start_row <= line - 1 and end_row >= line - 1 and node:type() == "method_definition" then
-        target_node = node
-        break
-    end
-  end
-
-  if not target_node then
+  if not parent_node then
     vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, { "No preview available" })
     return
   end
 
-  local method_text = vim.treesitter.get_node_text(target_node, bufnr)
+  local parent_node_text = vim.treesitter.get_node_text(parent_node, bufnr)
+  print("Parent node text: " .. parent_node_text)
 
-  vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(method_text, "\n"))
+  vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, vim.split(parent_node_text, "\n"))
 end
 
 M.function_picker = function(opts)
