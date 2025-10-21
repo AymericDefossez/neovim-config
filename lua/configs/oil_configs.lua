d276f18bdd393eb91745e17c0bc80de018905fad
Aymeric Defossez <aymeric.defossez@e-mothep.com>
2025-10-21 16:49:54 +0200

    fix(oil): add border and close security

--- lua/configs/oil_configs.lua
+++ lua/configs/oil_configs.lua
@@ -1,8 +1,15 @@
 return {
   float = {
     preview_split = "right",
+    border = "rounded",
   },
   keymaps = {
-    ["<Esc>"] = { "actions.close", mode = "n" }
-  }
+    ["<esc>"] = function ()
+      if not vim.bo.modified then
+        require("oil").close()
+      else
+        vim.notify("Des modifications non enregistrées existent.")
+      end
+    end,
+  },
 }
