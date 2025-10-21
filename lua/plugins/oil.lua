4472d7f35ba9baf23b1187367bbb05c8780248a9
Aymeric Defossez <aymeric.defossez@e-mothep.com>
2025-10-21 17:00:15 +0200

    refactor(oil): move configs and mappings into the plugin file

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,13 +1,26 @@
+local map = vim.keymap.set
+local opts = require("mappings.default_mappings_opts")
+
+map("n", "-", "<cmd>Oil --float<CR>", opts("Open Oil float window"))
+
 return {
   "stevearc/oil.nvim",
   dependencies = {
     "echasnovski/mini.icons",
   },
-  config = function()
-    local oil = require("oil")
-    local oil_configs = require("configs.oil_configs")
-
-    oil.setup(oil_configs)
-    require("mappings.oil_mappings")
-  end,
+  opts = {
+    float = {
+      preview_split = "right",
+      border = "rounded",
+    },
+    keymaps = {
+      ["<esc>"] = function ()
+        if not vim.bo.modified then
+          require("oil").close()
+        else
+          vim.notify("Des modifications non enregistrées existent.")
+        end
+      end,
+    },
+  },
 }
